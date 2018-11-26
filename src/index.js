2483d9a5d8ee1a29c3e6fd8309ad5e8c6ae34e6b
Jean <jeannguyen.ca@gmail.com>
2018-11-26 11:09:50 -0800

    Fix index

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
-import { unregister } from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
+// import { unregister } from "./serviceWorker";
 
 import "./style/custom.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-unregister();
+// unregister();
 
-// serviceWorker.register();
+serviceWorker.register();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
