0da23c61b48e2bcaf4d2873083b71409e7b12d5c
Jean <jeannguyen.ca@gmail.com>
2018-11-29 16:42:14 -0800

    Add https redirect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import HttpsRedirect from "react-https-redirect";
 
-import Header from './components/containers/Header'
-import Body from './components/containers/Body'
-import Footer from './components/containers/Footer'
+import Header from "./components/containers/Header";
+import Body from "./components/containers/Body";
+import Footer from "./components/containers/Footer";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Header></Header>
-          <Body></Body>
-          <Footer></Footer>
-        </div>
-      </BrowserRouter>
+      <HttpsRedirect>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+            <Body />
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </HttpsRedirect>
     );
   }
 }
